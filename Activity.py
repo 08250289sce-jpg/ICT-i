6c79bb32713d3a4a7efea0534345748b1b33e545
Wangchen Lhamo <08250289.sce@rub.edu.bt>
2026-03-20 16:16:53 +0600

    activity

---
+++ Activity.py
@@ -0,0 +1,14 @@
+print("Hello")
+name = input("enter their name:")
+num1 = float(input("enter first num:"));
+num2 = float(input("enter second num:"));
+additional_result = num1 + num2
+subtraction_result = num1  - num2
+multiplication_result = num1 * num2
+division_result = num1 / num2
+print(name)
+print(additional_result )
+print(subtraction_result)
+print(multiplication_result )
+print(division_result )
+print("Thank you")
