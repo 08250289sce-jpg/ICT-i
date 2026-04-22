4368ab397ee15e252a76faf6556dece957d8607d
Wangchen Lhamo <08250289.sce@rub.edu.bt>
2026-04-22 09:29:41 +0600

    defining finction

---
+++ Defining funchation.py
@@ -0,0 +1,10 @@
+def sum():
+    a = 5
+    b = 10
+    print("The sum of a and b is :", a+b)
+def product():
+    a = 5
+    b = 10
+    print ("The product of a and b is :", a*b)
+sum()
+product()
